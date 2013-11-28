0e71c4a3867f4a09e704fa453e16854d09cce0f3
Justin R. Miller <incanus@codesorcery.net>
2013-11-27 17:39:31 -0700

    basic JSON parse & show

--- MBCocoaPodsTemplate/MBCPTViewController.m
+++ MBCocoaPodsTemplate/MBCPTViewController.m
@@ -25,6 +25,30 @@
     self.mapView = [[RMMapView alloc] initWithFrame:self.view.bounds];
 
     [self.view addSubview:self.mapView];
+
+    NSDictionary *flight = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"flight" ofType:@"json"]]
+                                                           options:0
+                                                             error:nil];
+
+    NSMutableArray *points = [NSMutableArray array];
+
+    for (NSArray *coordinatePair in flight[@"features"][0][@"geometry"][@"coordinates"][0])
+    {
+        CLLocationDegrees lat = [coordinatePair[1] doubleValue];
+        CLLocationDegrees lon = [coordinatePair[0] doubleValue];
+
+        [points addObject:[[CLLocation alloc] initWithLatitude:lat longitude:lon]];
+    }
+
+    [self.mapView addAnnotation:[[RMPolylineAnnotation alloc] initWithMapView:self.mapView points:points]];
+
+    NSArray *bboxCoordinates = flight[@"features"][0][@"geometry"][@"bbox"];
+
+    CLLocationCoordinate2D ne = CLLocationCoordinate2DMake([bboxCoordinates[1] doubleValue], [bboxCoordinates[0] doubleValue]);
+    CLLocationCoordinate2D sw = CLLocationCoordinate2DMake([bboxCoordinates[3] doubleValue], [bboxCoordinates[2] doubleValue]);
+
+    [self.mapView zoomWithLatitudeLongitudeBoundsSouthWest:sw northEast:ne animated:NO];
+    self.mapView.zoom -= 1;
 }
 
 @end
